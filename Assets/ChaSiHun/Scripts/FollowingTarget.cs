07bff8c489f21ec3f4e58f38878f859a9e2845e6
chacorp <sihun.cha@gmail.com>
2020-10-07 17:27:06 +0900

    123321

---
+++ Assets/ChaSiHun/Scripts/FollowingTarget.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingTarget : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed = 8f;
+    public bool isPlayer;
+    GameObject player;
+
+    void Start()
+    {
+        // 만약 이게 플레이어라면 이걸 저장
+        if (isPlayer)
+        {
+            player = this.gameObject;
+        }
+        else
+        { 
+            //만약 플레이어가 아니라면 플레이어를 찾아서 저장
+            player = GameObject.Find("Player");
+        }
+    }
+
+    void playerflow()
+    {
+        if (!GameSceneManager.Instance.playerTube.activeSelf) { return; }
+
+        Vector3 direction = target.position - transform.position;
+        float distance = direction.magnitude;
+        //Vector3.Distance(target.position, transform.position);
+        if (distance > 1)
+        {
+            transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void npcFlow()
+    {
+    }
+
+    void Update()
+    {
+        if (isPlayer)
+        {
+            playerflow();
+        }
+
+        npcFlow();
+    }
+}
