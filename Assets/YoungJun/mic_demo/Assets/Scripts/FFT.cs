5fcb7cdb840f99803c103e8e4d7bf418fbd63421
lioninoil <34977559+lioninoil@users.noreply.github.com>
2020-10-10 16:08:27 +0900

    test_Dictation
    Testing DictationRecognizer API on windows10

---
+++ Assets/YoungJun/mic_demo/Assets/Scripts/FFT.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using B83.MathHelpers;
+
+//public class FFT : MonoBehaviour
+//{
+//    float[] spec = new float[1024];
+//    float[] tmp = new float[2048];
+//    Complex[] spec2 = new Complex[2048];
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        // Unity's FFT function
+//        AudioListener.GetSpectrumData(spec, 0, FFTWindow.Rectangular);
+//        for (int i = 0; i < spec.Length; i++)
+//        {
+//            Debug.DrawLine(new Vector3(i, 0), new Vector3(i, spec[i]), Color.cyan);
+//        }
+
+//        // My FFT based on the output samples.
+//        AudioListener.GetOutputData(tmp, 0);
+//        // copy the output data into the complex array
+//        for (int i = 0; i < tmp.Length; i++)
+//        {
+//            spec2[i] = new Complex(tmp[i], 0);
+//        }
+//        // calculate the FFT
+//        FFT.CalculateFFT(spec2, false);
+//        for (int i = 0; i < spec2.Length/2; i++)  // plot only the first half
+//        {
+//            // multiply the magnitde of each value by 2
+//            Debug.DrawLine(new Vector3(i, 4), new Vector3(i, 4+(float)spec2[i].magnitude+2), Color.white)
+//        }
+//    }
+//}
---
+++ Assets/YoungJun/mic_demo/Assets/Scripts/GetMic.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GetMic : MonoBehaviour
+{
+    public int audioSampleRate = 44100;
+    public string microphone;
+
+    private List<string> options = new List<string>();
+    private int samples = 8192;
+    public float[] spectrum = new float[1024];
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        foreach(string device in Microphone.devices)
+        {
+            if (microphone == null)
+            {
+                microphone = device;
+            }
+            options.Add(device);
+        }
+
+        microphone = options[PlayerPrefsManager.GetMicrophone()];
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdateMicrophone();
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Microphone.End(microphone);
+
+            // Play recorded audio, just for testing purpose.
+            //audioSource.Play();
+
+            GetSpectrumAudioSource();
+
+        }
+    }
+
+    void UpdateMicrophone()
+    {
+        Debug.Log("Start Recording");
+
+        audioSource.Stop();
+        // Start recording to audioclip from the mic
+        // .Start(device name, loop, int time, int freq)
+        audioSource.clip = Microphone.Start(microphone, true, 10, audioSampleRate);
+        //audioSource.loop = true;
+        //Debug.Log(down + "seconds has been recorded");
+
+        if (Microphone.IsRecording(microphone))
+        {
+            Debug.Log(microphone + " : ON AIR");
+        }
+        else Debug.Log(microphone + " : NOT being recorded");
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+    }
+
+}
