38e58b1676d63e26b65ff1bc97994d9feb24c059
Minki Hong <minki.hong@kaist.ac.kr>
2020-10-14 15:04:07 +0900

    Floater
    둥둥~

---
+++ Assets/Minki/Scripts/Floater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Floater : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+
+    private void FixedUpdate()
+    {
+        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
+
+        // 파도 높이보다 플레이어 높이가 낮으면
+        if (transform.position.y < waveHeight)
+        {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight-transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            // 부력 더해서 잠기지 않도록 보정
+            rigidbody.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
+
+        }
+
+
+    }
+
+}
