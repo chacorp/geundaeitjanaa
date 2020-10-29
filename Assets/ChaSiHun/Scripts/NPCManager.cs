3fdcc6601e6acd60d3245fa40f25e243b791c51f
chacorp <sihun.cha@gmail.com>
2020-10-29 20:33:10 +0900

    애니메이션 추가하고 씬 전환 !

--- Assets/ChaSiHun/Scripts/NPCManager.cs
+++ Assets/ChaSiHun/Scripts/NPCManager.cs
@@ -8,19 +8,21 @@ public class NPCManager : MonoBehaviour
     public List<Vector3> npcPos = new List<Vector3>();
 
     public float JumpP = 5f;
-    public float MoveP = 5f;
-    int n_idx = 0;
+    public float MoveP = 2.5f;
 
+    int n_idx = 0;
     float timer = 0;
+
     enum Sequence
     {
         jump,
         jumped,
         moveOn,
-        reset,
+        loop,
+        reset
     }
-    [SerializeField]
     Sequence Step;
+
     Transform npcJ_T;
     Rigidbody npcJ_R;
 
@@ -35,7 +37,7 @@ public class NPCManager : MonoBehaviour
     }
     void NPCJUMP()
     {
-        if (GameSceneManager.Instance.startButtonClicked) return;
+        if (GameSceneManager.Instance.startButtonClicked) Step = Sequence.reset;
 
         switch (Step)
         {
@@ -62,26 +64,50 @@ public class NPCManager : MonoBehaviour
                 npcWait.Remove(npcJ_T);
 
                 // 4. 뒤에 녀석들 하나씩 앞으로 땡기기
-                //Vector3 dir = npcPos[n_idx] - npcWait[n_idx].position;
-                //npcWait[n_idx].transform.position += dir * Time.deltaTime;
-                // npcWait[n_idx].Translate(dir * Time.deltaTime);
-                npcWait[n_idx].transform.position = Vector3.Lerp(npcWait[n_idx].transform.position, npcWait[n_idx].position, MoveP * Time.deltaTime);
-                if (npcWait[n_idx].position == npcPos[n_idx])
+                npcWait[n_idx].transform.position = Vector3.Lerp(npcWait[n_idx].transform.position, npcPos[n_idx], MoveP * Time.deltaTime);
+
+                if (Vector3.Distance(npcWait[n_idx].position, npcPos[n_idx]) < 1)
                 {
                     npcWait[n_idx].position = npcWait[n_idx].position;
                     n_idx++;
-                    Step = n_idx >= npcWait.Count ? Sequence.reset : Sequence.moveOn;
+
+                    // 다음 단계로 넘기기
+                    Step = n_idx >= npcWait.Count ? Step++ : Step;
                 }
                 break;
 
-            case Sequence.reset:
+            case Sequence.loop:
                 // 5. 점프한 첫번째 맨 뒤로 옮기기
                 npcWait.Add(npcJ_T);
                 npcJ_T.position = npcPos[npcWait.IndexOf(npcJ_T)];
+
+                // 6. 인덱스 리셋
+                n_idx = 0;
+                Step = Sequence.jump;
+                break;
+
+            case Sequence.reset:
+                // 점프 뛴 녀석이 있다면
+                if (npcJ_T != null)
+                {
+                    // 다시 대기열에 넣어주고
+                    npcWait.Add(npcJ_T);
+                    // 위치 리셋!
+                    foreach (Transform n in npcWait) n.position = npcPos[npcWait.IndexOf(n)];
+                    // 뛴 녀석은 이제 없다
+                    npcJ_T = null;
+                }
                 break;
         }
     }
 
+    void MainMenu()
+    {
+        if (GameSceneManager.Instance.startButtonClicked)
+        {
+
+        }
+    }
 
     void Update()
     {
