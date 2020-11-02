9b41ba13681fe1a4f126004ac602ddb0bfd64deb
Minki Hong <minki.hong@kaist.ac.kr>
2020-11-02 23:09:46 +0900

    Revert "Merge branch 'master' of https://github.com/chacorp/geundaeitjanaa"
    This reverts commit 3a796cbba36c6bd3500970389bde7353242b0f09, reversing
    changes made to 24a12f3516944681c9d43ff1843a87a310b31132.

--- Assets/ChaSiHun/Scripts/RPCManager.cs
+++ Assets/ChaSiHun/Scripts/RPCManager.cs
@@ -25,23 +25,15 @@ public class RPCManager : MonoBehaviour
     // 플레이어
     Transform playerObj;
 
-    // 다른 플레이어의 캐릭터
-    GameObject rpc;
+    // 다른 플레이어의 캐릭터들 담아둘 리스트
+    List<GameObject> RPC_activeFalse = new List<GameObject>();
+    List<GameObject> RPC_activeTrue = new List<GameObject>();
 
     // 따라갈 녀석
     Transform follower;
 
-    public enum npc_State
-    {
-        None,
-        GetCloser,
-        Stay,
-        TakePhoto,
-        GetAway
-    }
-    public npc_State npc_A;
-
-    float approachSpeed = 1.5f;
+    bool isApproaching = false;
+    float approachingSpeed = 1.5f;
 
     public Transform spawnPoint;
     public Transform targetPoint;
@@ -52,61 +44,38 @@ public class RPCManager : MonoBehaviour
         for (int i = 0; i < MaxRPCs; i++)
         {
             // 1. 다른 플레이어의 캐릭터를 만들어서,
-            rpc = Instantiate(RPC_prefab);
+            GameObject rpc = Instantiate(RPC_prefab);
 
-            // 2. 비활성화하기
+            // 2. 비활성화하고
             rpc.SetActive(false);
+
+            // 3. 리스트에 넣어두기
+            RPC_activeFalse.Add(rpc);
         }
 
         // 오브젝트 가져오기
         playerObj = GameSceneManager.Instance.player.transform;
         follower = GameObject.FindGameObjectWithTag("Respawn").transform;
-
-
-        npc_A = npc_State.None;
     }
 
     // 플레이어에게 접근하기!!
-    void Approach()
+    void GetApproach()
     {
+        // rpc 하나 가져오기
+        GameObject rpc = RPC_activeTrue[0];
+
         // 타겟과의 거리가 일정 거리 이상이면 접근하고, 일정 거리 미만이면 그만 접근하기
         float distance = Vector3.Distance(playerObj.position, rpc.transform.position);
 
         if (distance >= 3f)
         {
-            rpc.transform.position = Vector3.Lerp(rpc.transform.position, playerObj.position, approachSpeed * Time.deltaTime);
+            rpc.transform.position = Vector3.Lerp(rpc.transform.position, playerObj.position, approachingSpeed * Time.deltaTime);
         }
         else
         {
             rpc.transform.SetParent(follower);
-            // 그만 접근하기
-            npc_A = npc_State.Stay;
-            GameSceneManager.Instance.currentScene = GameSceneManager.Scenes.MatchFound;
-        }
-    }
-
-
-    void RunAway()
-    {
-        // 탈출 포인트 가져오기
-        Transform leavePoint = follower.GetChild(1);
-
-        // 타겟과의 거리가 일정 거리 이상이면 접근하고, 일정 거리 미만이면 비활성화하기
-        float distance = Vector3.Distance(leavePoint.position, rpc.transform.position);
-        if (distance >= 2f)
-        {
-            rpc.transform.position = Vector3.Lerp(rpc.transform.position, leavePoint.position, approachSpeed * Time.deltaTime);
-        }
-        else
-        {
-            Prefab_Float rpcPF = rpc.GetComponentInChildren<Prefab_Float>();
-            rpcPF.EndFloating();
-            rpc.transform.SetParent(null);
-            rpc.SetActive(false);
-
-            // 그만 접근하기
-            npc_A = npc_State.None;
-            GameSceneManager.Instance.currentScene = GameSceneManager.Scenes.FindMatch;
+            // 접근하기 취소
+            isApproaching = false;
         }
     }
 
@@ -114,6 +83,25 @@ public class RPCManager : MonoBehaviour
     {
         if (currentPlayer < 0) currentPlayer = 0;
 
+        // 시작했을 때만 입력받기
+        if (GameSceneManager.Instance.currentScene == GameSceneManager.Scenes.FindMatch)
+        {
+            // currentRPC = 참여하고 있는 플레이어 수 가져오기
+            if (Input.GetKeyDown(KeyCode.A) && currentPlayer < MaxRPCs)
+            {
+                currentPlayer++;
+                GameSceneManager.Instance.currentScene = GameSceneManager.Scenes.MatchFound;
+            }
+        }
+
+        if (GameSceneManager.Instance.currentScene == GameSceneManager.Scenes.MatchFound)
+        {
+            if (Input.GetKeyDown(KeyCode.D) && currentPlayer > 0)
+            {
+                currentPlayer--;
+                GameSceneManager.Instance.currentScene = GameSceneManager.Scenes.FindMatch;
+            }
+        }
 
         // 모든 플레이어의 숫자 확인
         if (currentPlayer != previousPlayer)
@@ -121,21 +109,33 @@ public class RPCManager : MonoBehaviour
             // 다른 플레이어가 입장했을때
             if (currentPlayer > previousPlayer)
             {
+                // 리스트에서 캐릭터 하나 빼와서 다른 리스트에 넣기
+                GameObject rpc = RPC_activeFalse[0];
+                RPC_activeFalse.Remove(rpc);
+                RPC_activeTrue.Add(rpc);
+
                 // 적당한 위치에 놓고
                 rpc.transform.position = spawnPoint.position;
 
                 // 활성화 
                 rpc.SetActive(true);
-
-                // 상태 전환하기
-                npc_A = npc_State.GetCloser;
+                isApproaching = true;
             }
 
             // 다른 플레이어가 빠져나갔을때
             if (currentPlayer < previousPlayer)
             {
                 // 플레이어에게 접근 취소
-                npc_A = npc_State.TakePhoto;
+                isApproaching = false;
+
+                GameObject rpc = RPC_activeTrue[0];
+                RPC_activeTrue.Remove(rpc);
+                RPC_activeFalse.Add(rpc);
+
+                Prefab_Float rpcPF = rpc.GetComponentInChildren<Prefab_Float>();
+                rpcPF.EndFloating();
+                rpc.transform.SetParent(null);
+                rpc.SetActive(false);
             }
 
             // 이전 플레이어 수 동기화
@@ -143,19 +143,6 @@ public class RPCManager : MonoBehaviour
         }
 
         // RPC를 플레이어에게 접근시키기
-        switch (npc_A)
-        {
-            case npc_State.GetCloser:
-                Approach();
-                break;
-
-            case npc_State.TakePhoto:
-                GameSceneManager.Instance.Take_A_Photo();
-                break;
-
-            case npc_State.GetAway:
-                RunAway();
-                break;
-        }
+        if (isApproaching) GetApproach();
     }
 }
