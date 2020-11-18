b44b2caae01b3d416dbdb819d412af5b5165659f
Minki Hong <minki.hong@kaist.ac.kr>
2020-11-19 02:11:21 +0900

    아이템 줍기
    BallLauncher 코드와 함께 사용할 코드입니다.
    
    튜브를 던진 후 아이템에 맞으면 둘 다 사라지는 코드입니다.

---
+++ Assets/Minki/Scripts/PickupObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Box collider를 젠된 아이템과 플레이어가 던질 튜브에 추가, is Trigger 버튼 활성화
+public class PickupObject : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            print ("Item picked up");
+            Destroy(gameObject);
+        }
+    }
+}
